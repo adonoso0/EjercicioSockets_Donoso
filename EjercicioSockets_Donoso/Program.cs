e29ea1a359c53d4be35330a8ecf8841d48b52aa0
adonoso0 <agustin.donoso02@inacapmail.cl>
2023-05-02 20:26:00 -0400

    update

--- EjercicioSockets_Donoso/Program.cs
+++ EjercicioSockets_Donoso/Program.cs
@@ -1,15 +1,59 @@
-﻿using System;
+﻿using EjercicioSockets_Donoso.Comunicacion;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EjercicioSockets_Donoso
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            int puerto = Int32.Parse(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine("Iniciando Servidor en puerto {0}", puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+            if (servidor.Iniciar())
+            {
+                while (true)
+                {
+                    //Obtener clientes
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Esperando Clientes...");
+                    if (servidor.ObtenerCliente())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Conexion Establecida!");
+                        //Protocolo de comunicacion
+                        string mensaje = "";
+                        while (mensaje.ToLower() != "chao")
+                        {
+                            //Leo el mensaje del Cliente
+                            mensaje = servidor.Leer();
+                            Console.WriteLine("C:{0}", mensaje);
+                            if (mensaje.ToLower() != "chao")
+                            {
+                                //El cliente espera una respuesta
+                                Console.WriteLine("Digame lo que quiere decir guruguru");
+                                mensaje = Console.ReadLine().Trim();
+                                Console.WriteLine("S:{0}", mensaje);
+                                servidor.Escribir(mensaje);
+                            }
+                        }
+                        servidor.CerrarConexion();
+
+
+                    }
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No es posible iniciar servidor");
+                Console.ReadKey();
+            }
         }
     }
 }
